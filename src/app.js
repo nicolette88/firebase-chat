0686371108ae643412fe259bbbbae0f3936e8f9f
Niki <nicolette88@gmail.com>
2021-06-25 15:41:32 +0200

    Órai feladatok

---
+++ src/app.js
@@ -0,0 +1,96 @@
+import './index.html';
+import './scss/style.scss';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import config from './db_config.js';
+import scrollIntoView from 'scroll-into-view-if-needed';
+
+
+const firedb = firebase.initializeApp(config);
+const db = firedb.firestore();
+
+
+async function sendMessage(data) {
+  const res = await db.collection('messages').add(data);
+  document.querySelector('#message').value = '';
+  console.log(res);
+}
+
+function displayMessage(message) {
+  const messageDOM = `
+    <div class="message">
+      <i class="fas fa-user"></i>
+      <div>
+        <span class="username">${message.username}
+          <time>20:12 PM</time>
+        </span>
+        <br>
+        <span class="message-text">${message.message}
+        </span>
+      </div>
+      <div class="message-edit-buttons">
+        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-pen"></i>
+      </div>
+    </div>
+`;
+  document.querySelector('#messages').insertAdjacentHTML('beforeend', messageDOM);
+  scrollIntoView(document.querySelector('#messages'), {
+    scrollMode: 'if-needed',
+    block: 'end'
+  });
+}
+
+function createMessage() {
+  const message = document.querySelector('#message').value;
+  const username = document.querySelector('#nickname').value;
+  const date = firebase.firestore.Timestamp.fromDate(new Date());
+  // ha a változó neve ugyanaz mint a key amit létre akarunk hozni az objectben, akkor nem kell kétszer kiírni
+  return { message, username, date };
+}
+
+async function displayAllMessages() {
+  const query = await db.collection('messages').orderBy('date', 'asc').get();
+  query.forEach((doc) => {
+    displayMessage(doc.data());
+  });
+}
+
+function handleMessage() {
+  const message = createMessage();
+  if (message.username && message.message) {
+    sendMessage(message);
+    // displayMessage(message);
+  }
+}
+
+// amikor  html teljesen betölt:
+window.addEventListener('DOMContentLoaded', () => {
+  // displayAllMessages();
+  document.querySelector('#send').addEventListener('click', () => {
+    handleMessage();
+  });
+});
+
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    handleMessage();
+  }
+});
+
+// listen for changes in the database
+db.collection('messages').orderBy('date', 'asc')
+  .onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        displayMessage(change.doc.data());
+      }
+      if (change.type === 'modified') {
+        console.log('Modified message: ', change.doc.data());
+      }
+      if (change.type === 'removed') {
+        console.log('Removed message: ', change.doc.data());
+      }
+    });
+  });
